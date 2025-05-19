7ddef8ba93d2b16e6365db380bb4a57422dd3b94
tekinha16 <00001116025887sp@al.educacao.sp.gov.br>
2025-05-19 14:27:32 -0300

    criamos a funcionalidade de salvar um arquivo

--- exercicio_07.py
+++ exercicio_07.py
@@ -1,36 +1,51 @@
-clientes = []
+db_clientes = "db_clientes.json"
+# clientes = []
+import json
+import os
+def carregar_dados():
+    if os.path.exists(db_clientes):
+        with open(db_clientes, "r", encoding="utf-8") as arq_json:
+            return json.load(arq_json)
+    else:
+        return [] 
 
+ 
 def obter_dados_cliente():
-    nome_cliente = input("Informe o Nome completo: ")
-    cpf_cliente = int(input("Informe o CPF do cliente: "))
-    rg_cliente = int(input("Informe o RG do cliente: "))
-    nascimento_cliente = input("Informe a Data de Nascimento do Cliente: ")
-    endereco_cliente = input("Informe o Endereço do Cliente: ")
-    cidade_cliente = input("Informe a Cidade do Cliente: ")
-    estado_cliente = input("Informe o Estado do Cliente: ")
-    telefone_cliente = int(input("Informe oTelefone do Cliente: "))
-    celular_cliente = int(input("Informe o Celular do Cliente: "))
-    email_cliente = input("Informe o Email do Cliente: ")
+    nome_cliente = input("Informe o Nome Completo do Cliente")
+    cpf_cliente = int(input("Informe o CPF Completo do Cliente"))
+    rg_cliente = int(input("Informe o  Numero de RG Completo do Cliente"))
+    data_de_nascimento_cliente = input("Informe a Data de Nascimento do Cliente")
+    endereco_cliente = input("Informe o Endereço do Cliente")
+    cidade_cliente = input("Informe a Cidade do Cliente")
+    estado_cliente = input("Informe o Estado do Cliente")
+    telefone_cliente = int(input("Informe o Numero de Telefone do Cliente"))
+    celular_cliente = int(input("Informe o Numero de Celular do Cliente"))
+    email_cliente = input("Informe o E-mail Completo do Cliente")
 
     cliente = {
-            "nome_cliente": nome_cliente,
-            "cpf_cliente": cpf_cliente,
-            "rg_cliente": rg_cliente,
-            "nascimento_cliente": nascimento_cliente,
-            "endereco_cliente": endereco_cliente,
-            "cidade_cliente": cidade_cliente,
-            "estado_cliente": estado_cliente,
-            "telefone_cliente": telefone_cliente,
-            "celular_cliente": celular_cliente,
-            "email_cliente": email_cliente
-    
-     }
-    
-    return clientes
-def cadastrar_cliente(dados_cliete):
-    clientes.append(dados_cliete)
+        "nome_cliente": nome_cliente,
+        "cpf_cliente": cpf_cliente,
+        "rg_cliente": rg_cliente,
+        "data_de_nascimento_cliente": data_de_nascimento_cliente,
+        "endereco_cliente": endereco_cliente,
+        "cidade_cliente": cidade_cliente,
+        "estado_cliente": estado_cliente,
+        "telefone_cliente": telefone_cliente,
+        "celular_cliente": celular_cliente,
+        "email_cliente": email_cliente
 
-    return clientes
+    }
+
+    return cliente
+
+def cadastrar_cliente(dados_cliente):
+    clientes = carregar_dados()
+    clientes.append(dados_cliente)
+
+    with open(db_clientes, "w" , encoding="utf-8") as arq_json:
+        json.dump(clientes, arq_json, indent=4, ensure_ascii=False)
+
+
 
 def mostrar_dados_clientes():
     for cliente in obter_dados_cliente:
@@ -47,23 +62,25 @@ def mostrar_dados_clientes():
               """)
         
 def iniciar_sistema():
-            while True:
-                print("")
-                print("="*80)
-                print("Opcao 1 - Mostrar Lista de Clientes")
-                print("Opcao 2 - Cadastrar Clientes")
-                print("Opcao 3 - Sair do Sistema.")
-                print("="*80)
+    clientes = carregar_dados()
+    while True:
+        
+        print("")
+        print("="*80)
+        print("Opcao 1 - Mostrar Lista de Clientes")
+        print("Opcao 2 - Cadastrar Clientes")
+        print("Opcao 3 - Sair do Sistema.")
+        print("="*80)
 
-                opcao = input("Escolha uma das opções do Menu: ")
+        opcao = input("Escolha uma das opções do Menu: ")
 
-                if opcao == "1":
-                    mostrar_dados_clientes(clientes)
-                elif opcao == "2":
-                    cadastrar_cliente(obter_dados_cliente())
-                elif opcao == "3":
-                    print("Sistema Finalizado")
-                    break
-                else:
-                    print("Opcao invalida, escolha uma das opções no menu.")
+        if opcao == "1":
+            mostrar_dados_clientes(clientes)
+        elif opcao == "2":
+            cadastrar_cliente(obter_dados_cliente())
+        elif opcao == "3":
+            print("Sistema Finalizado")
+            break
+        else:
+            print("Opcao invalida, escolha uma das opções no menu.")
 iniciar_sistema()
